12cb65f004503f4bb6ded020b4afb935990f13db
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-05-02 20:08:03 +0900

    Item, User 엔티티 정의

--- src/main/java/com/example/myitemstock/entity/Item.java
+++ src/main/java/com/example/myitemstock/entity/Item.java
@@ -1,4 +1,68 @@
 package com.example.myitemstock.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+@Entity
 public class Item {
+    @Id
+    private Long id;
+    private String name;
+    private String brand;
+    private Long price;
+    private String picture;
+    private Long amount;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
 }
--- src/main/java/com/example/myitemstock/entity/User.java
+++ src/main/java/com/example/myitemstock/entity/User.java
@@ -1,4 +1,65 @@
 package com.example.myitemstock.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Entity
 public class User {
+    @Id
+    private Long id;
+    private String email;
+    private String nickname;
+    private String passwd;
+
+    @OneToMany
+    private Set<Item> items;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item){
+           this.items.remove(item);
+    }
 }
