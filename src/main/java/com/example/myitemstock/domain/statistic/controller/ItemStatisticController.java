92e14194e612338d54fddd564e70c97dfbadacd6
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-06-02 14:55:16 +0900

    패키지 구조를 도메인별로 분할해서 정리

--- src/main/java/com/example/myitemstock/domain/statistic/controller/ItemStatisticController.java
+++ src/main/java/com/example/myitemstock/domain/statistic/controller/ItemStatisticController.java
@@ -1,20 +1,17 @@
-package com.example.myitemstock.domain.itemlist.controller;
+package com.example.myitemstock.domain.statistic.controller;
 
 import com.example.myitemstock.domain.itemlist.model.ItemListRequest;
 import com.example.myitemstock.domain.itemlist.model.ItemListResponse;
 import com.example.myitemstock.entity.Item;
-import com.example.myitemstock.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ItemListController {
-    @GetMapping("/api/user/item")
-    public ResponseEntity getItemList(@RequestBody ItemListRequest itemListRequest){
+public class ItemStatisticController {
+    @GetMapping("/api/user/item/statistic")
+    public ResponseEntity getItemStatistic(@RequestBody ItemListRequest itemListRequest) {
         Item item1 = Item.builder()
                 .name("Item1")
                 .amount(2L)
@@ -43,17 +40,4 @@ public class ItemListController {
         return new ResponseEntity(itemListResponse, HttpStatus.OK);
 
     }
-
-    /**
-     * 요구 : 정보를 받아 특정 유저에 아이템을 등록해주세요
-     * 조건 : 만약 해당 유저가 없는 경우 실패를 반환해주세요
-     */
-    @PostMapping("/api/user/item")
-    public Item addItem(@RequestBody User user, @RequestBody Item item){
-        return null;
-    }
-
-
-
-
 }
